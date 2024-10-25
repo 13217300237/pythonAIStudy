f9246b24a7cb0c1997e3c8b3c8dfa32ea49ad6c9
13217300237 <545679452@qq.com>
2024-10-26 00:27:42 +0800

    langchain作业提交

--- 0929langChain/作业/rag+手动管理记忆demo.py
+++ 0929langChain/作业/rag+手动管理记忆demo.py
@@ -5,6 +5,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.messages import HumanMessage, AIMessage
+import datetime
 
 memoryList = []
 
@@ -20,6 +21,16 @@ vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbedd
 
 # k is the number of chunks to retrieve
 retriever = vectorstore.as_retriever(k=4)
+
+
+def log(*args):
+    # 获取当前时间并格式化为字符串
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # 将所有传入的参数连接成一个字符串
+    message = " ".join(map(str, args))
+    # 打印时间戳和日志消息
+    print(f"[{current_time}] {message}")
 
 
 def ask(question):
@@ -46,7 +57,7 @@ def ask(question):
     )
     chat = ChatOpenAI(model="gpt-4o-mini")
     document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
-
+    log("AI正在思考......")
     result = document_chain.invoke(
         {
             "context": docs,
@@ -54,11 +65,18 @@ def ask(question):
         }
     )
 
-    print("AI的回答是", result)
+    log("AI的回答是", result)
     memoryList.append(AIMessage(result))
 
 
 if __name__ == "__main__":
-    ask("缘分居能做什么")
-    ask("缘分居能测评qq号吉凶么")
-    ask("我刚才问了你什么问题")
+    log("聊天机器人已启动，输入 exit退出")
+    while True:
+        # 接收用户输入
+        user_input = input("用户提问：")
+
+        # 检查用户输入是否为 "exit"
+        if user_input.lower() == "exit":
+            print("程序已退出。")
+            break
+        ask(user_input)
